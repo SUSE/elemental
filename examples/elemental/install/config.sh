cd8c545619a2d28797f46f0a202c9bad6a184183
Fredrik Lönnegren <fredrik.lonnegren@suse.com>
2026-05-27 09:49:09 +0200

    Fix InsecureIgnoreHostKey code-scanning warning
    Adds a hostkey in tests/assets/ and adds the key to the tested systems
    in the integration tests.
    
    The hostkey is matched using FixedHostKey callback in the SUT
    clientConfig.
    
    Signed-off-by: Fredrik Lönnegren <fredrik.lonnegren@suse.com>

--- examples/elemental/install/config.sh
+++ examples/elemental/install/config.sh
@@ -12,3 +12,20 @@ echo "linux" | passwd root --stdin
 # Allow root ssh access (for testing purposes only!)
 echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/root_access.conf
 systemctl enable sshd
+
+# Static host-key (for testing purposes only!)
+cat > /etc/ssh/ssh_host_ecdsa_key <<EOF
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAaAAAABNlY2RzYS
+1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQQw5slj5JGbABTKEU9Ca7rLeZYom0mi
+kPjpDxOw05Eg76gt0Ub6Tnc3JMxGIfA3meiUhGj+fF61tjbfcGu8TDzcAAAAqMKQaBbCkG
+gWAAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDDmyWPkkZsAFMoR
+T0Jrust5liibSaKQ+OkPE7DTkSDvqC3RRvpOdzckzEYh8DeZ6JSEaP58XrW2Nt9wa7xMPN
+wAAAAgYsNrbSuJR2TC3+h+0rthmq2uRhFrq7m0F9KZHF4gKuQAAAANZnJlbG9uQGF0b21p
+YwECAw==
+-----END OPENSSH PRIVATE KEY-----
+EOF
+
+cat > /etc/ssh/ssh_host_ecdsa_key.pub <<EOF
+ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDDmyWPkkZsAFMoRT0Jrust5liibSaKQ+OkPE7DTkSDvqC3RRvpOdzckzEYh8DeZ6JSEaP58XrW2Nt9wa7xMPNw= user@elemental-vm
+EOF
